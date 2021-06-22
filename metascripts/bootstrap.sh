b0e459d47e45a4669621a48625142075f3ac7086
kaiserdrake <salunga.norman@gmail.com>
2021-06-22 09:27:12 +0900

    Move preconfigure

--- metascripts/bootstrap.sh
+++ metascripts/bootstrap.sh
@@ -1,6 +1,18 @@
 # dotfiles boostrap
 SRCLOC=`dirname $0`
 
+preconfigure()
+{
+  # Set default shell to bash
+  echo "Setting default shell to 'bash'..."
+  echo dash dash/sh boolean false | sudo debconf-set-selections
+  sudo DEBIAN_FRONTEND=noninteractive dpkg-reconfigure dash
+
+  echo "Updating package library..."
+  sudo apt-get --yes update > /dev/null
+  sudo DEBIAN_FRONTEND=noninteractive apt-get --yes upgrade > /dev/null
+}
+
 # create symbolic link
 load_symlink()
 {
@@ -38,10 +50,12 @@ unload_symlink()
 
 
 if [ "$1" == "uninstall" ]; then
+  preconfigure
   sh $SRCLOC/install-gentools.sh uninstall
   sh $SRCLOC/install-nvim.sh uninstall
   unload_symlink
 else
+  preconfigure
   sh $SRCLOC/install-gentools.sh
   sh $SRCLOC/install-nvim.sh nightly
   load_symlink
--- metascripts/install-gentools.sh
+++ metascripts/install-gentools.sh
@@ -9,18 +9,6 @@ packageList=(
   universal-ctags
 )
 
-preconfigure()
-{
-  # Set default shell to bash
-  echo "Setting default shell to 'bash'..."
-  echo dash dash/sh boolean false | sudo debconf-set-selections
-  sudo DEBIAN_FRONTEND=noninteractive dpkg-reconfigure dash
-
-  echo "Updating package library..."
-  sudo apt-get --yes update > /dev/null
-  sudo DEBIAN_FRONTEND=noninteractive apt-get --yes upgrade > /dev/null
-}
-
 install()
 {
   # Install common development tools
@@ -43,7 +31,6 @@ uninstall()
 if [ "$1" == "uninstall" ]; then
   uninstall
 else
-  preconfigure
   install
 fi
 
