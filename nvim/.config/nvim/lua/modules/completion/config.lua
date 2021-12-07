47277ddfc8358230c454f55f98b7e136ba18435c
Norman Salunga <salunga.norman@gmail.com>
2021-12-07 10:06:19 +0900

    Add gutentags excludes

--- nvim/.config/nvim/lua/modules/completion/config.lua
+++ nvim/.config/nvim/lua/modules/completion/config.lua
@@ -8,7 +8,7 @@ function config.nvim_compe()
     require'compe'.setup {
         enabled = true;
         debug = false;
-        min_length = 2;
+        min_length = 3;
         preselect = 'disable';
         max_abbr_width = 60;
         max_kind_width = 60;
@@ -51,6 +51,54 @@ function config.gutentags()
     vim.g.gutentags_generate_on_missing = 1
     vim.g.gutentags_generate_on_write = 1
     vim.g.gutentags_generate_on_empty_buffer = 0
+    vim.g.gutentags_ctags_exclude = {
+        '*.git', '*.svg', '*.hg',
+        '*/tests/*',
+        'build',
+        'dist',
+        'debbuild',
+        '*sites/*/files/*',
+        'bin',
+        'node_modules',
+        'bower_components',
+        'cache',
+        'compiled',
+        'docs',
+        'example',
+        'bundle',
+        'vendor',
+        '*.md',
+        '*-lock.json',
+        '*.lock',
+        '*bundle*.js',
+        '*build*.js',
+        '.*rc*',
+        '*.json',
+        '*.min.*',
+        '*.map',
+        '*.bak',
+        '*.zip',
+        '*.pyc',
+        '*.class',
+        '*.sln',
+        '*.Master',
+        '*.csproj',
+        '*.tmp',
+        '*.csproj.user',
+        '*.cache',
+        '*.pdb',
+        'tags*',
+        'cscope.*',
+        '*.css',
+        '*.less',
+        '*.scss',
+        '*.exe', '*.dll',
+        '*.mp3', '*.ogg', '*.flac',
+        '*.swp', '*.swo',
+        '*.bmp', '*.gif', '*.ico', '*.jpg', '*.png',
+        '*.rar', '*.zip', '*.tar', '*.tar.gz', '*.tar.xz', '*.tar.bz2',
+        '*.pdf', '*.doc', '*.docx', '*.ppt', '*.pptx',
+    }
 end
 
 return config
