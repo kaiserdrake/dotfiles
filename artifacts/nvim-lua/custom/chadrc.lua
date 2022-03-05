56712bf199b3677bbd837dbb489a59f6e31a1556
kaiserdrake <salunga.norman@gmail.com>
2022-03-05 15:43:44 +0900

    Add mappings for gitsigns

--- artifacts/nvim-lua/custom/chadrc.lua
+++ artifacts/nvim-lua/custom/chadrc.lua
@@ -40,6 +40,17 @@ M.mappings.plugins = {
   telescope = {
     file_browser = "<leader>fe",
     find_notes = "<leader>fn",
+  },
+  gitsigns = {
+    next_hunk = "[g",
+    prev_hunk = "]g",
+    stage_hunk = "<leader>gs",
+    undo_stage_hunk = "<leader>gu",
+    reset_hunk = "<leader>gr",
+    preview_hunk = "<leader>gv",
+    blame_line = "<leader>gb",
+    toggle_blame = "<leader>gB",
+    toggle_signs = "<leader>gT",
   }
 }
 
--- artifacts/nvim-lua/custom/mappings.lua
+++ artifacts/nvim-lua/custom/mappings.lua
@@ -13,3 +13,15 @@ map("n", maps.shortcuts.diag_next, ":lua vim.lsp.diagnostic.goto_next()<CR>")
 -- Telescope
 map("n", plugin_maps.telescope.file_browser, ":Telescope file_browser<CR>")
 map("n", plugin_maps.telescope.find_notes, ":Telescope find_files find_command=rg,-uu,--files,--follow cwd="..os.getenv("FILESTORE_PATH").."/orgs<CR>")
+
+-- Gitsigns
+map("n", plugin_maps.gitsigns.next_hunk, ":Gitsigns next_hunk<CR>")
+map("n", plugin_maps.gitsigns.prev_hunk, ":Gitsigns prev_hunk<CR>")
+map("n", plugin_maps.gitsigns.stage_hunk, ":Gitsigns stage_hunk<CR>")
+map("n", plugin_maps.gitsigns.undo_stage_hunk, ":Gitsigns undo_stage_hunk<CR>")
+map("n", plugin_maps.gitsigns.preview_hunk, ":Gitsigns preview_hunk<CR>")
+map("n", plugin_maps.gitsigns.reset_hunk, ":Gitsigns reset_hunk<CR>")
+map("n", plugin_maps.gitsigns.blame_line, ":Gitsigns blame_line<CR>")
+map("n", plugin_maps.gitsigns.toggle_blame, ":Gitsigns toggle_current_line_blame<CR>")
+map("n", plugin_maps.gitsigns.toggle_signs, ":Gitsigns toggle_signs<CR>")
+
