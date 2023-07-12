a38644ec93a2b9d67e1025cafc8b3a558fb42e60
kaisedrake <salunga.norman@gmail.com>
2023-07-12 22:15:04 +0900

    Reverse filter for removing disabled plugs

--- artifacts/nvim-lua/custom/chadrc.lua
+++ artifacts/nvim-lua/custom/chadrc.lua
@@ -14,35 +14,10 @@ M.mappings = require "custom.mappings"
 M.lazy_nvim = {
   performance = {
     rtp = {
-      disabled_plugins = {
-        "2html_plugin",
-        "tohtml",
-        "getscript",
-        "getscriptPlugin",
-        "gzip",
-        "logipat",
-        "netrw",
-        "netrwPlugin",
-        "netrwSettings",
-        "netrwFileHandlers",
-        "matchit",
-        "tar",
-        "tarPlugin",
-        "rrhelper",
-        "spellfile_plugin",
-        "vimball",
-        "vimballPlugin",
-        "zip",
-        "zipPlugin",
-        "tutor",
-        "rplugin",
-        "syntax",
-        "synmenu",
-        "optwin",
-        "compiler",
-        "bugreport",
-        "ftplugin",
-      },
+      disabled_plugins = vim.tbl_filter(function(plug)
+        return not vim.tbl_contains({ "editorconfig" }, plug)
+      end,
+      require("plugins.configs.lazy_nvim").performance.rtp.disabled_plugins),
     },
   },
 }
