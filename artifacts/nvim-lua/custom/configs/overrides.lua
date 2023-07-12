cf82ccc09c1bffded77b2cc8b72657f6c4bddc2f
kaisedrake <salunga.norman@gmail.com>
2023-07-12 21:48:34 +0900

    Move lazy overrides to chadrc

--- artifacts/nvim-lua/custom/configs/overrides.lua
+++ artifacts/nvim-lua/custom/configs/overrides.lua
@@ -76,40 +76,4 @@ M.mason = {
   },
 }
 
-M.lazy_nvim = {
-  performance = {
-    rtp = {
-      disabled_plugins = {
-        "2html_plugin",
-        "tohtml",
-        "getscript",
-        "getscriptPlugin",
-        "gzip",
-        "logipat",
-        "netrw",
-        "netrwPlugin",
-        "netrwSettings",
-        "netrwFileHandlers",
-        "matchit",
-        "tar",
-        "tarPlugin",
-        "rrhelper",
-        "spellfile_plugin",
-        "vimball",
-        "vimballPlugin",
-        "zip",
-        "zipPlugin",
-        "tutor",
-        "rplugin",
-        "syntax",
-        "synmenu",
-        "optwin",
-        "compiler",
-        "bugreport",
-        "ftplugin",
-      },
-    },
-  },
-}
-
 return M
