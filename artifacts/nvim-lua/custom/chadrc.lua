0ac9024a58dccd60b223b2e1c5970bcd5e24afc9
Norman Salunga <norman.salunga@excelfore.com>
2024-01-19 23:28:11 +0900

    use pos instead of ln in sattusline

--- artifacts/nvim-lua/custom/chadrc.lua
+++ artifacts/nvim-lua/custom/chadrc.lua
@@ -1,4 +1,5 @@
 local M = {}
+local fn = vim.fn
 
 M.plugins = "custom.plugins"
 
@@ -8,7 +9,21 @@ M.ui = {
     Comment = { italic = true },
   },
   statusline = {
-    theme = "vscode_colored"
+    theme = "vscode_colored",
+    overriden_modules = function(modules)
+      modules[9] = (function()
+
+        local current_line = fn.line(".", vim.g.statusline_winid)
+        local total_line = fn.line("$", vim.g.statusline_winid)
+        local text = math.modf((current_line / total_line) * 100) .. tostring "%%"
+        text = string.format("%4s", text)
+
+        text = (current_line == 1 and "Top") or text
+        text = (current_line == total_line and "Bot") or text
+
+        return vim.o.columns > 80 and ("%#StText# Ln " .. text .. ", Col %c  ") or (text .. " ")
+      end)()
+    end,
   },
   cheatsheet = {
     theme = "simple"
