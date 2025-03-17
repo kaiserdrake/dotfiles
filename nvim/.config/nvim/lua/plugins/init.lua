c8b36636d24419414970046bd3016a0629c64685
Norman Salunga <kaiserdrake@gmail.com>
2025-03-17 12:55:53 +0900

    Add whitespace highlight

--- nvim/.config/nvim/lua/plugins/init.lua
+++ nvim/.config/nvim/lua/plugins/init.lua
@@ -25,4 +25,21 @@ return {
     end,
   },
 
+  -- highlight whitespace
+  {
+    'johnfrankmorgan/whitespace.nvim',
+    lazy = false,
+    config = function ()
+      require('whitespace-nvim').setup({
+        -- `highlight` configures which highlight is used to display
+        -- trailing whitespace
+        highlight = 'DiffDelete',
+        -- `ignored_filetypes` configures which filetypes to ignore when
+        -- displaying trailing whitespace
+        ignored_filetypes = { 'TelescopePrompt', 'Trouble', 'help', 'dashboard' },
+        ignore_terminal = true,
+        return_cursor = true,
+      })
+    end,
+  },
 }
--- zsh/.zshrc
+++ zsh/.zshrc
@@ -78,6 +78,7 @@ zstyle ':completion:*' list-colors "${(s.:.)LS_COLORS}"
 zstyle ':completion:*' menu no
 zstyle ':fzf-tab:complete:cd:*' fzf-preview 'ls --color $realpath'
 zstyle ':fzf-tab:complete:__zoxide_z:*' fzf-preview 'ls --color $realpath'
+zstyle ':fzf-tab:*' fzf-bindings 'tab:accept'
 
 # Aliases
 alias ls='ls --color'
